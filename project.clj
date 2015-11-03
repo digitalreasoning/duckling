0fadce244a8ea091a8af10eab5a7fb41566479d9
Jared Bunting <jared.bunting@digitalreasoning.com>
2015-11-03 13:04:51 -0600

    Setting version to 0.3.9-drs2 for release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject wit/duckling "0.3.9-drs2-SNAPSHOT"
+(defproject wit/duckling "0.3.9-drs2"
   :description "Date & Number parser"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
